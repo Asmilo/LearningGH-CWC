83e7a8eefce6df60a0908904c1a4353399a465fd
MichielN <Asmilo@home.nl>
2021-04-17 21:15:41 +0200

    Initial Commit

---
+++ LearningGHwithCWC/LearningGHwithCWCApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHwithCWCApp.swift
+//  LearningGHwithCWC
+//
+//  Created by Michiel Nooij on 17/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHwithCWCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
